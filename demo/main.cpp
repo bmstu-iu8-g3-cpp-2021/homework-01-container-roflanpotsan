02f98e486d1d5e61f20463b80fe332b5126f178c
roflanpotsan <l1sZ@ya.ru>
2022-05-12 08:09:48 +0300

    finished

--- demo/main.cpp
+++ demo/main.cpp
@@ -6,7 +6,7 @@ int main() {
   unordered_set<int> x{5, 6, 7, 4,88,8,10,9,23}, y, z(x), d;
   for (auto i : x) std::cout << i << " ";
   std::cout << '\n';
-  std::cout << *std::find_if(x.begin(), x.end(), [](int val){return val % 2 == 0;});
-  std::for_each(x.begin(), x.end(), [](int val){if(val % 2 == 0) std::cout << val << '\n';});
+  //std::cout << *std::find_if(x.begin(), x.end(), [](int val){return val % 2 == 0;});
+  //std::for_each(x.begin(), x.end(), [](int val){if(val % 2 == 0) std::cout << val << '\n';});
 
 }
